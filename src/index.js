267a48fced4333e84ad1d040d8539f9fcf5ca16e
Toby Cox <eagerterrier@gmail.com>
2017-02-21 14:55:00 +0000

    ENH: first
    Signed-off-by: Toby Cox <eagerterrier@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,101 +1,93 @@
-/* eslint-disable  func-names */
-/* eslint quote-props: ["error", "consistent"]*/
-/**
- * This sample demonstrates a simple skill built with the Amazon Alexa Skills
- * nodejs skill development kit.
- * This sample supports multiple lauguages. (en-US, en-GB, de-DE).
- * The Intent Schema, Custom Slots and Sample Utterances for this skill, as well
- * as testing instructions are located at https://github.com/alexa/skill-sample-nodejs-fact
- **/
-
 'use strict';
 
 const Alexa = require('alexa-sdk');
 
 var http       = require("https")
-  , APP_ID     = "amzn1.ask.skill.1371f436-66d4-4b57-b1a1-b1700e3e9607"
-  , TFL_KEY    = "a2a0722e5dafa0984d493e6a5423ce04"
-  , TFL_APP_ID = "88eef1cb"
+  , APP_ID     = ""
+  , TFL_KEY    = ""
+  , TFL_APP_ID = ""
   , currentStation = ""
+  , currentDirection = "outbound"
+  , debug = false
   , currentStation_id = null;
 
 const languageStrings = {
     'en-GB': {
         translation: {
-            FACTS: [
-                'A year on Mercury is just 88 days long.',
-                'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-                'Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.',
-                'On Mars, the Sun appears about half the size as it does on Earth.',
-                'Earth is the only planet not named after a god.',
-                'Jupiter has the shortest day of all the planets.',
-                'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-                'The Sun contains 99.86% of the mass in the Solar System.',
-                'The Sun is an almost perfect sphere.',
-                'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-                'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-                'The temperature inside the Sun can reach 15 million degrees Celsius.',
-                'The Moon is moving approximately 3.8 cm away from our planet every year.',
-            ],
-            SKILL_NAME: 'British Space Facts',
-            GET_FACT_MESSAGE: "Here's your fact: ",
-            HELP_MESSAGE: 'You can say tell me a space fact, or, you can say exit... What can I help you with?',
+            SINGLE_AVAILABLE_TRAIN : 'The next train is in',
+            MULTIPLE_AVAILABLE_TRAINS : 'The next trains are in',
+            NO_AVAILABLE_TRAINS : '',
+            FOUND_TRAINS : 'I have found some trains for you.',
+            AND : 'and',
+            SKILL_NAME: 'Next Trains on the London Overground',
+            HELP_MESSAGE: 'You can say get the next train from and then say a station name, so for instance get the next train from Homerton',
             HELP_REPROMPT: 'What can I help you with?',
-            STOP_MESSAGE: 'Goodbye!',
+            STOP_MESSAGE: 'Have a safe journey!',
         },
     }
 };
 
 var stopsAsDefined = {"homerton" : "910GHOMRTON", "kentish town west" : "910GKNTSHTW"};
 
+
 var url = function(stopId){
-  return "https://api-argon.tfl.gov.uk/Line/london-overground/Arrivals?stopPointId=" + stopId + "&app_key=" + TFL_KEY + "&app_id=" + TFL_APP_ID;
+    return "https://api-argon.tfl.gov.uk/Line/london-overground/Arrivals?stopPointId=" + stopId + "&app_key=" + TFL_KEY + "&app_id=" + TFL_APP_ID;
 };
 
 var stationlookup = function(stationName){
-  console.log('requesting data from...');
-  console.log("https://api-argon.tfl.gov.uk/StopPoint/Search?query=" + encodeURIComponent(stationName) + "&modes=overground&app_key=" + TFL_KEY + "&app_id=" + TFL_APP_ID);
-  return "https://api-argon.tfl.gov.uk/StopPoint/Search?query=" + encodeURIComponent(stationName) + "&modes=overground&app_key=" + TFL_KEY + "&app_id=" + TFL_APP_ID;
+    return "https://api-argon.tfl.gov.uk/StopPoint/Search?query=" + encodeURIComponent(stationName) + "&modes=overground&app_key=" + TFL_KEY + "&app_id=" + TFL_APP_ID;
 };
 
+var log = function(log_what){
+    if (!debug) return;
+    console.log(log_what);
+}
+
 var getJsonFromMta = function(stopId, callback){
-  http.get(url(stopId), function(res){
-    var body = "";
-    res.on("data", function(data){
-      body += data;
-    });
+    log('requesting getJsonFromMta');
+    
+    http.get(url(stopId), function(res){
+        
+        var body = "";
+        res.on("data", function(data){
+          body += data;
+        });
 
-    res.on("end", function(){
-      var result = JSON.parse(body);
-      callback(result);
-    });
+        res.on("end", function(){
+          var result = JSON.parse(body);
+          callback(result);
+        });
 
-  }).on("error", function(e){
-    console.log("Error: " + e);
-  });
+    }).on("error", function(e){
+        console.log("Error: " + e);
+    });
 };
 
 var getStationId = function(stationName, callback){
-  console.log('getting a station - '  + stationName);
-  http.get(stationlookup(stationName), function(res){
-    var body = "";
+    log('getting a station - '  + stationName);
+  
+    var req = http.request(stationlookup(stationName), (res) => {
 
-    res.on("data", function(data){
-      body += data;
+        var body = '';
+
+        res.on('data', (d) => {
+            body += d;
+        });
+
+        res.on('end', function () {
+            callback(body);
+        });
+
     });
+    req.end();
 
-    res.on("end", function(){
-      var result = JSON.parse(body);
-      callback(result);
+    req.on('error', (e) => {
+        console.error(e);
     });
-
-  }).on("error", function(e){
-    console.log("Error: " + e);
-  });
 };
 
 var secondsToMinutes = function(seconds){
-	var minutes = Math.floor(parseInt(seconds, 10) / 60);
+    var minutes = Math.floor(parseInt(seconds, 10) / 60);
 	return minutes;
 };
 
@@ -104,65 +96,76 @@ var returnSingleOrPluralMinutes = function(minutes){
 };
 
 var array_key_exists = function(_key, _array){
-	for(var key in _array){
-	    if(_array[key] == _key){
-			return true;
-		}
-	}
-	return false;
+	return _array.hasOwnProperty(_key);
 };
 
-var handleNextTrainRequest = function(intent, session, response){
-  currentStation = intent.slots.station.value;
-  if (!array_key_exists(currentStation.toLowerCase(), stopsAsDefined)) {
-  	getStationId(currentStation.toLowerCase(), function(data){
-    	if(data.matches){
-    		getTrainTimes(data.matches[0].id, intent, session, response);
-    	} else {
-    	  var text = "That station does not exist."
-    	  var cardText = text;
-    	}
-    });
-  } else {
-    getTrainTimes(stopsAsDefined[currentStation.toLowerCase()], intent, session, response);
-  }
-};
-
-var getTrainTimes = function(stationId, intent, session, response){
-  console.log("getting train times for station id " + stationId);
-  getJsonFromMta(stationId, function(data){
-    if(data[0].expectedArrival){
-      var outboundtrains = new Array();
-      var cardText = "The next trains are in : ";
-      for (i = 0; i < data.length; i++) {
-      	if (data[i].direction == "outbound") {
-      		outboundtrains.push(data[i].timeToStation);
-      	}
-      }
-      outboundtrains.sort(function(a, b){return a-b});
-      console.log(outboundtrains);
-      for (j = 0; j < outboundtrains.length; j++) {
-      	var minutes = secondsToMinutes(outboundtrains[j]);
-      	cardText += minutes + " " + returnSingleOrPluralMinutes(minutes);
-      	if (j < (outboundtrains.length - 2)) {
-      		cardText += ", ";
-      	} else if (j < (outboundtrains.length - 1)) {
-      		cardText += " and ";
-      	}
-      }
-    } else {
-      var text = "There are no trains from " + currentStation + " until tomorrow morning.";
-      var cardText = text;
-    }
-
-    var heading = "Next train for stop: " + currentStation;
-    response.tellWithCard(text, heading, cardText);
+var ucwords = function(string){
+    return (string + '').replace(/^([a-z\u00E0-\u00FC])|\s+([a-z\u00E0-\u00FC])/g, function ($1) {
+      return $1.toUpperCase()
     });
 }
 
-var TrainSchedule = function(){
-  AlexaSkill.call(this, APP_ID);
-};
+var getTrainTimes = function(stationId, intent, session, response, that){
+
+    log("getting train times for station id " + stationId);
+
+    getJsonFromMta(stationId, function(data){
+        if(data[0].expectedArrival){
+            var outboundtrains = new Array();
+            var outboundtrainDestinations = new Array();
+            var lastTrainDestination = '';
+            var totalTrainDestinations = 0;
+            var trainDestination = '';
+            var text = that.t('FOUND_TRAINS');
+            var cardText = ((data.length == 1) ? that.t('SINGLE_AVAILABLE_TRAIN') : that.t('MULTIPLE_AVAILABLE_TRAINS')) + " : ";
+            for (var i = 0; i < data.length; i++) {
+            	if (data[i].direction == currentDirection) {
+            		outboundtrains.push(data[i].timeToStation);
+            		trainDestination = data[i].destinationName.replace(' (London) Rail Station', '');
+            		outboundtrainDestinations.push(trainDestination);
+            		if (lastTrainDestination != trainDestination) {
+            		    totalTrainDestinations++;
+            		}
+            		lastTrainDestination = trainDestination;
+            	}
+            }
+            outboundtrains.sort(function(a, b){return a-b});
+            log(outboundtrains);
+            for (var j = 0; j < outboundtrains.length; j++) {
+                var minutes = secondsToMinutes(outboundtrains[j]);
+            	cardText += minutes + " " + returnSingleOrPluralMinutes(minutes);
+            	if (j < (outboundtrains.length - 2)) {
+            		cardText += ", ";
+            	} else if (j < (outboundtrains.length - 1)) {
+            		cardText += " " + that.t('AND') + " ";
+            	}
+            }
+            console.log('totalTrainDestinations: ' + totalTrainDestinations);
+            console.log('outboundtrainDestinations length: ' + outboundtrainDestinations.length);
+            if (totalTrainDestinations != 1) {
+                cardText += ' in the direction of ';
+                for (var k = 0; k < outboundtrainDestinations.length; k++) {
+                	cardText += outboundtrainDestinations[k];
+                	if (k < (outboundtrainDestinations.length - 2)) {
+                		cardText += ", ";
+                	} else if (k < (outboundtrainDestinations.length - 1)) {
+                		cardText += " " + that.t('AND') + " ";
+                	}
+                }
+                cardText += ' respectively';
+            } else {
+                cardText += ' all heading for ' + outboundtrainDestinations[0];
+            }
+            text = text + '\n\n' + cardText;
+        } else {
+            var text = "There are no trains from " + ucwords(currentStation) + " until tomorrow morning.";
+            var cardText = text;
+        }
+    
+        var heading = "Next train for stop: " + ucwords(currentStation);
+        that.emit(':tellWithCard', cardText, heading, text);
+    });
+}
 
 
 const handlers = {
@@ -173,12 +176,31 @@ const handlers = {
         this.emit('handleNextTrainRequest');
     },
     'handleNextTrainRequest': function () {
-        currentStation = this.event.request.intent.slots.station.value;
-        console.log('currentstation: ' + currentStation);
-
-        // Create speech output
-        //const speechOutput = this.t('GET_FACT_MESSAGE') + randomFact;
-        //this.emit(':tellWithCard', speechOutput, this.t('SKILL_NAME'), randomFact);
+        if (!this.event.request.intent.slots.station.hasOwnProperty('value')) {
+            const speechOutput = this.t('HELP_MESSAGE');
+            const reprompt = this.t('HELP_MESSAGE');
+            this.emit(':ask', speechOutput, reprompt);
+        } else {
+            currentStation = this.event.request.intent.slots.station.value;
+            currentDirection = (this.event.request.intent.slots.hasOwnProperty('inboundOrOutbound') && this.event.request.intent.slots.inboundOrOutbound.hasOwnProperty('value')) ? this.event.request.intent.slots.inboundOrOutbound.value : 'outbound';
+            log('currentstation: ' + currentStation);
+            log('currentDirection: ' + currentDirection);
+            log('stopsAsDefined: ');
+            log(stopsAsDefined);
+            if (!array_key_exists(currentStation.toLowerCase(), stopsAsDefined)) {
+            	var tflResponse = getStationId(currentStation.toLowerCase(), function(data){
+              	if(data.matches){
+              		getTrainTimes(data.matches[0].id, this.event.request.intent, this.event.request.session, this.event.request.response, this);
+              	} else {
+              	  var text = "That station does not exist."
+              	  var cardText = text;
+              	}
+              });
+            } else {
+              var tflResponse = getTrainTimes(stopsAsDefined[currentStation.toLowerCase()], this.event.request.intent, this.event.request.session, this.event.request.response, this);
+            }
+            log(tflResponse);
+        }
     },
     'Unhandled': function () {
         const speechOutput = this.t('HELP_MESSAGE');
@@ -203,9 +225,10 @@ const handlers = {
 
 exports.handler = (event, context) => {
     const alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
+    alexa.appId = APP_ID;
     // To enable string internationalization (i18n) features, set a resources object.
     alexa.resources = languageStrings;
+    //alexa.dynamoDBTableName = '';
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
